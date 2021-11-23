bfe05ed9e2e743afee9156acf38ef25d839c7fd6
baicha <chengnan0107@163.com>
2021-11-23 18:51:31 +0800

    feat:新增dialog组件的配置

--- src/components/Dialogpage/Dialogdemo.vue
+++ src/components/Dialogpage/Dialogdemo.vue
@@ -1,15 +1,36 @@
 <template>
   <div>
-    <h1>这是dialog 示例</h1>
-    <Dialog>这是Dialog </Dialog>
+    <h1>这是Dialog 示例</h1>
+    <Button @click="onShow">点击</Button>
+    <Dialog
+      v-model:visible="visible"
+      :onCloseOverlay="false"
+      title="这是标题"
+      okText="确定"
+      cancelText="取消"
+    >
+      <h4>这是Dialog可配置内容</h4>
+    </Dialog>
   </div>
 </template>
 <script lang="ts">
 import Dialog from "../../lib/Dialog.vue";
+import { ref } from "vue";
+import Button from "../../lib/Button.vue";
 export default {
   components: {
     Dialog,
+    Button,
   },
-  setup() {},
+  setup() {
+    const visible = ref(false);
+    const onShow = () => {
+      visible.value = true;
+    };
+    return {
+      visible,
+      onShow,
+    };
+  },
 };
 </script>
