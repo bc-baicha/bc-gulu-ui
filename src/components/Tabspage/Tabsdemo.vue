a6bd6311e494c30175e1314cb67acdacef9c24e2
baicha <chengnan0107@163.com>
2021-12-08 15:55:44 +0800

    fix:完善tabs组件

--- src/components/Tabspage/Tabsdemo.vue
+++ src/components/Tabspage/Tabsdemo.vue
@@ -1,15 +1,24 @@
 <template>
   <div>
-    <h1>这是 tabs示例</h1>
-    <Tabs>这是tabs组件</Tabs>
+    <h1>这是Tabs示例</h1>
+    <Tabs v-model:selected="x">
+      <Tab title="导航1">内容1</Tab>
+      <Tab title="导航2">内容2</Tab>
+    </Tabs>
   </div>
 </template>
 <script lang="ts">
 import Tabs from "../../lib/Tabs.vue";
+import Tab from "../../lib/Tab.vue";
+import { ref } from "vue";
 export default {
   components: {
     Tabs,
+    Tab,
   },
-  setup() {},
+  setup() {
+    const x = ref("导航1");
+    return { x };
+  },
 };
 </script>
