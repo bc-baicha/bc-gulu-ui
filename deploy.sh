e5a6094acafb3df06d7825d8c84e0baecbe4ef1f
baicha <chengnan0107@163.com>
2021-12-23 15:28:15 +0800

    feat:添加一键部署

---
+++ deploy.sh
@@ -0,0 +1,11 @@
+rm -rf dist &&
+pnpm run build &&
+cd dist && 
+git init &&
+git add . &&
+git commit -m 'fix:修改的内容' &&
+git branch -M main &&
+git remote add origin git@github.com:bc-baicha/baicha-ui-website.git &&
+git push -f -u origin main &&
+cd -
+echo https://bc-baicha.github.io/baicha-ui-website
