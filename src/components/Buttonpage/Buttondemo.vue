430945a6959921ac7e4f1b76cb6d1135dffc4050
baicha <chengnan0107@163.com>
2021-12-27 19:00:39 +0800

    feat:添加theme

--- src/components/Buttonpage/Buttondemo.vue
+++ src/components/Buttonpage/Buttondemo.vue
@@ -1,16 +1,20 @@
 <template>
   <h1>Button 按钮</h1>
   <Showcodedemo :component="Buttonsize" />
+  <Showcodedemo :component="Buttontype" />
 </template>
 <script lang="ts">
 import Buttonsize from "./components/Buttonsize.vue";
+import Buttontype from "./components/Buttontype.vue";
 export default {
   components: {
     Buttonsize,
+    Buttontype,
   },
   setup() {
     return {
       Buttonsize,
+      Buttontype,
     };
   },
 };
--- src/lib/Button.vue
+++ src/lib/Button.vue
@@ -5,6 +5,7 @@
 </template>
 <script lang="ts">
 import { computed } from "vue";
+import "./styles/button.scss";
 export default {
   props: {
     theme: {
@@ -36,48 +37,3 @@ export default {
   },
 };
 </script>
-<style lang="scss">
-$hover: #1890ff;
-.bc-button {
-  padding: 10px 20px;
-  text-align: center;
-  background-color: #fff;
-  border: solid 1px #ccc;
-  border-radius: 2px;
-  cursor: pointer;
-  margin-right: 20px;
-  font-size: 15px;
-  font-weight: 400;
-}
-.bc-button:focus {
-  border-color: $hover;
-  color: $hover;
-  animation: boxShadowColor 0.5s linear 1;
-}
-@keyframes boxShadowColor {
-  0% {
-    box-shadow: 0px 0px 1px;
-  }
-  25% {
-    box-shadow: 0px 0px 2px;
-  }
-  50% {
-    box-shadow: 0px 0px 3px;
-  }
-  75% {
-    box-shadow: 0px 0px 4px;
-  }
-  100% {
-    box-shadow: 0px 0px 1px;
-  }
-}
-.bc-button:hover {
-  color: $hover;
-}
-.bc-large {
-  padding: 5px 10px;
-}
-.bc-big {
-  padding: 12px 35px;
-}
-</style>
