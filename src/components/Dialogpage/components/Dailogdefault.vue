e809309102779da7173803be74c614553efacf75
baicha <chengnan0107@163.com>
2021-12-30 17:10:11 +0800

    feat:修改一些样式

---
+++ src/components/Dialogpage/components/Dailogdefault.vue
@@ -0,0 +1,32 @@
+<demo>
+Dialog基础用法
+</demo>
+<template>
+  <Button theme="primay" @click="onChangeVisible"> 点击 </Button>
+  <Dialog v-model:visible="visible" :onCloseOverlay="true">
+    <template v-slot:content>
+      <div>这是内容</div>
+    </template>
+  </Dialog>
+</template>
+<script lang="ts">
+import { Dialog } from "../../../lib/index";
+import { ref } from "vue";
+import { Button } from "../../../lib/index";
+export default {
+  components: {
+    Dialog,
+    Button,
+  },
+  setup() {
+    const visible = ref(false);
+    const onChangeVisible = () => {
+      visible.value = true;
+    };
+    return {
+      visible,
+      onChangeVisible,
+    };
+  },
+};
+</script>
--- src/lib/Dialog.vue
+++ src/lib/Dialog.vue
@@ -5,16 +5,15 @@
       <div class="bc-dialog-wrapper">
         <div class="bc-dialog">
           <header>
-            <!-- {{ title }}  -->
-            <slot name="title" />
+            {{ title }}
             <span @click="onClose" class="bc-dialog-close"></span>
           </header>
           <main>
             <slot name="content" />
           </main>
           <footer>
-            <Button @click="onClose">{{ okText }}</Button>
             <Button @click="onClose">{{ cancelText }}</Button>
+            <Button theme="primay" @click="onClose">{{ okText }}</Button>
           </footer>
         </div>
       </div>
@@ -22,6 +21,7 @@
   </template>
 </template>
 <script lang="ts">
+import "./styles/dialog.scss";
 import Button from "./Button.vue";
 export default {
   props: {
@@ -33,17 +33,17 @@ export default {
       type: Boolean,
       default: true,
     },
-    // title: {
-    //   type: String,
-    //   default: "标题",
-    // },
+    title: {
+      type: String,
+      default: "标题",
+    },
     cancelText: {
       type: String,
       default: "Cancel",
     },
     okText: {
       type: String,
-      default: "ok",
+      default: "OK",
     },
   },
   components: {
@@ -65,69 +65,3 @@ export default {
   },
 };
 </script>
-<style lang="scss" scoped>
-$radius: 4px;
-$border-color: #d9d9d9;
-.bc-dialog {
-  background: white;
-  border-radius: $radius;
-  box-shadow: 0 0 3px fade_out(black, 0.5);
-  min-width: 15em;
-  max-width: 90%;
-  &-overlay {
-    position: fixed;
-    top: 0;
-    left: 0;
-    width: 100%;
-    height: 100%;
-    background: fade_out(black, 0.5);
-    z-index: 10;
-  }
-  &-wrapper {
-    position: fixed;
-    left: 50%;
-    top: 50%;
-    transform: translate(-50%, -50%);
-    z-index: 11;
-  }
-  > header {
-    padding: 12px 16px;
-    border-bottom: 1px solid $border-color;
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
-    font-size: 20px;
-  }
-  > main {
-    padding: 12px 16px;
-  }
-  > footer {
-    border-top: 1px solid $border-color;
-    padding: 12px 16px;
-    text-align: right;
-  }
-  &-close {
-    position: relative;
-    display: inline-block;
-    width: 16px;
-    height: 16px;
-    cursor: pointer;
-    &::before,
-    &::after {
-      content: "";
-      position: absolute;
-      height: 1px;
-      background: black;
-      width: 100%;
-      top: 50%;
-      left: 50%;
-    }
-    &::before {
-      transform: translate(-50%, -50%) rotate(-45deg);
-    }
-    &::after {
-      transform: translate(-50%, -50%) rotate(45deg);
-    }
-  }
-}
-</style>
