bc2f236496b6248e941f1cb2d039dc33b9268f81
baicha <chengnan0107@163.com>
2022-01-07 18:16:47 +0800

    feat:dialog添加背景使用组件

--- src/components/Dialogpage/Dialogdemo.vue
+++ src/components/Dialogpage/Dialogdemo.vue
@@ -1,6 +1,7 @@
 <template>
   <h1>Dialog 组件</h1>
   <Showcodedemo :component="Dialogdefault" />
+  <Showcodedemo :component="Dialogmask" />
   <Showcodedemo :component="Dialogwidth" />
   <Showcodedemo :component="Dialoginfo" />
 </template>
@@ -8,17 +9,20 @@
 import Dialogdefault from "./components/Dialogdefault.vue";
 import Dialogwidth from "./components/Dialogwidth.vue";
 import Dialoginfo from "./components/Dialoginfo.vue";
+import Dialogmask from "./components/Dialogmask.vue";
 export default {
   components: {
     Dialogdefault,
     Dialogwidth,
     Dialoginfo,
+    Dialogmask,
   },
   setup() {
     return {
       Dialogdefault,
       Dialogwidth,
       Dialoginfo,
+      Dialogmask,
     };
   },
 };
---
+++ src/components/Dialogpage/components/Dialogmask.vue
@@ -0,0 +1,32 @@
+<demo>
+去掉背景点击关闭弹框
+</demo>
+<template>
+  <Button theme="primay" @click="onChangeVisible">点击</Button>
+  <Dialog v-model:visible="visible" :onCloseOverlay="false">
+    <template v-slot:content>
+      <div>这是内容</div>
+    </template>
+  </Dialog>
+</template>
+<script lang="ts">
+import { Dialog } from "../../../lib/index";
+import { ref } from "vue";
+import { Button } from "../../../lib/index";
+export default {
+  components: {
+    Dialog,
+    Button,
+  },
+  setup() {
+    const visible = ref(false);
+    const onChangeVisible = () => {
+      visible.value = true;
+    };
+    return {
+      visible,
+      onChangeVisible,
+    };
+  },
+};
+</script>
