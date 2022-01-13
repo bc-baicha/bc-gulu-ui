3aed10202573fbb5836569ad36451d307888e645
baicha <chengnan0107@163.com>
2022-01-13 17:46:56 +0800

    fix:添加css

--- src/components/Collapsepage/components/Collapsedefault.vue
+++ src/components/Collapsepage/components/Collapsedefault.vue
@@ -2,14 +2,25 @@
 支持disable
 </demo>
 <template>
-  <Collapse />
+  <Collapse :header="obj.header" :content="obj.content" :maxHeight="300" />
+  <Collapse :header="obj.header" :content="obj.content" :maxHeight="300" />
 </template>
 <script lang="ts">
+import { reactive } from "@vue/reactivity";
 import { Collapse } from "../../../lib/index";
 export default {
   components: {
     Collapse,
   },
-  setup() {},
+  setup() {
+    const obj = reactive({
+      header: "这是头部",
+      content:
+        "A dog is a type of domesticated animal.Known for its loyalty and faithfulness,it can be found as a welcome guest in many households across the world.A dog is a type of domesticated animal.Known for its loyalty and faithfulness,it can be found as a welcome guest in many households across the world.A dog is a type of domesticated animal.Known for its loyalty and faithfulness,it can be found as a welcome guest in many households across the world.A dog is a type of domesticated animal.Known for its loyalty and faithfulness,it can be found as a welcome guest in many households across the world.A dog is a type of domesticated animal.Known for its loyalty and faithfulness,it can be found as a welcome guest in many households across the world.A dog is a type of domesticated animal.Known for its loyalty and faithfulness,it can be found as a welcome guest in many households across the world.A dog is a type of domesticated animal.Known for its loyalty and faithfulness,it can be found as a welcome guest in many households across the world.A dog is a type of domesticated animal.Known for its loyalty and faithfulness,it can be found as a welcome guest in many households across the world.A dog is a type of domesticated animal.Known for its loyalty and faithfulness,it can be found as a welcome guest in many households across the world.",
+    });
+    return {
+      obj,
+    };
+  },
 };
 </script>
--- src/lib/Collapse.vue
+++ src/lib/Collapse.vue
@@ -1,4 +1,54 @@
-<template>这是</template>
+<template>
+  <div class="bc-coll-wrap">
+    <div @click="changeShow" class="coll-header">
+      <svg
+        class="point-left"
+        :style="{ transform: show ? 'rotate(90deg)' : '' }"
+      >
+        <use xlink:href="#icon-point-right" />
+      </svg>
+      {{ header }}
+    </div>
+    <div :class="show ? 'show-content' : 'hidden-content'" ref="heightContent">
+      <div :class="show ? 'show-opcity' : 'hidden-opcity'">{{ content }}</div>
+    </div>
+  </div>
+</template>
 <script lang="ts">
-export default {};
+import { ref } from "vue";
+import "./styles/collapse.scss";
+export default {
+  props: {
+    header: {
+      type: String,
+      default: "",
+    },
+    content: {
+      type: String,
+      default: "",
+    },
+    maxHeight: {
+      type: Number,
+      default: 100,
+    },
+  },
+  setup(props) {
+    const { maxHeight } = props;
+    const show = ref(false);
+    const heightContent = ref<HTMLDivElement>(null);
+    const changeShow = () => {
+      show.value = !show.value;
+      if (show.value) {
+        heightContent.value.style.height = maxHeight + "px";
+      } else {
+        heightContent.value.style.height = 0 + "px";
+      }
+    };
+    return {
+      show,
+      changeShow,
+      heightContent,
+    };
+  },
+};
 </script>
